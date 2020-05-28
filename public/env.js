72045b69bc054b9694bba03b6ff9650281026171
alessio bonti <alexbonti83@hotmail.com>
2020-05-28 18:11:57 +1000

    update

--- public/env.js
+++ public/env.js
@@ -1,4 +1,19 @@
+const urlAnalyze='/analyze'
+
+let analyze=function(doc){
+  $.get(urlAnalyze,{text:doc},function(result){
+    console.log(JSON.parse(result))
+    $('#output').val(JSON.parse(result).result.originalText)
+
+  })
+}
+
 $(document).ready(function(){
   console.log('Ready')
+  $('#analyze').click(function(){
+    let input=$('#input').val();
+    console.log(input)
+    analyze(input)
+  })
 
 })
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require("express"),
     app = express();
 
-var port = process.env.PORT || 8080;
+const req = require('request');
+
+   
+
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,6 +13,17 @@ app.get("/sayHello", function (request, response) {
   var user_name = request.query.user_name;
   response.end("Hello " + user_name + "!");
 });
+
+app.get('/analyze',function(request,response){
+  var text= encodeURI(request.query.text);
+  console.log(text)
+  reqObject="http://localhost:8080/analyze?text="+text;
+  req(reqObject,  (err, result, body) => {
+    if (err) { return console.log(err); }
+    console.log(result.body)
+    response.end(result.body)
+  });
+})
 
 app.listen(port);
 console.log("Listening on port ", port);
